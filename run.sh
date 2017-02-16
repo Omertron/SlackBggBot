3aaba00ebb79e17fa9d5c4b730aa2b17a790a031
Stuart Boston <omertron@gmail.com>
2017-02-16 21:58:37 +0000

    Add log files

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-git reset --hard HEAD
-git pull
-mvn clean compile assembly:single
-java -classpath .\target\* com.omertron.slackbot.SlackBot
 
+while true; do
+  git pull
+  mvn clean compile assembly:single
+  java -jar .\target\slack-bot-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+  if [ $? -eq 1 ]
+  then
+    # restart
+  else
+    # exit
+    break
+  fi
+done
