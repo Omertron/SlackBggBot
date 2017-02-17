d35914d7c15109447a5d62f99934886f46b32285
Stuart Boston <omertron@gmail.com>
2017-02-17 14:35:04 +0000

    Add copyright messages

--- src/main/java/com/omertron/slackbot/listeners/HelpListener.java
+++ src/main/java/com/omertron/slackbot/listeners/HelpListener.java
@@ -1,3 +1,22 @@
+/*
+ *      Copyright (c) 2017 Stuart Boston
+ *
+ *      This file is part of the BGG Slack Bot.
+ *
+ *      The BGG Slack Bot is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      The BGG Slack Bot is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with the BGG Slack Bot.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package com.omertron.slackbot.listeners;
 
 import static com.omertron.slackbot.Constants.DELIM_LEFT;
@@ -100,6 +119,7 @@ public class HelpListener implements SlackMessagePostedListener {
             helpMessage.setFallback("Help commads for the bot");
             helpMessage.setPretext("The following commands are available from the game bot");
             helpMessage.addMarkdownIn("fields");
+            helpMessage.setColor("good");
 
             for (HelpInfo hi : INFO) {
                 helpMessage.addField(String.format("%1$s <%2$s>", hi.command, hi.param), hi.message, false);
