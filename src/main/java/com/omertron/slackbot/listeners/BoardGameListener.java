91889d248aa8543843e5eb76c929b858bf7853fe
Stuart Boston <omertron@gmail.com>
2017-02-20 13:52:05 +0000

    Update for API changes

--- src/main/java/com/omertron/slackbot/listeners/BoardGameListener.java
+++ src/main/java/com/omertron/slackbot/listeners/BoardGameListener.java
@@ -21,14 +21,18 @@ package com.omertron.slackbot.listeners;
 
 import com.omertron.bgg.BggApi;
 import com.omertron.bgg.BggException;
-import com.omertron.bgg.model.SearchWrapper;
-import com.omertron.bgg.model.Thing;
+import com.omertron.bgg.enums.IncludeExclude;
 import com.omertron.bgg.model.BoardGameExtended;
 import com.omertron.bgg.model.CollectionItem;
 import com.omertron.bgg.model.CollectionItemWrapper;
 import com.omertron.bgg.model.IdValue;
 import com.omertron.bgg.model.RankedList;
+import com.omertron.bgg.model.SearchWrapper;
+import com.omertron.bgg.model.Thing;
 import com.omertron.bgg.model.UserInfo;
+import com.omertron.slackbot.Constants;
+import static com.omertron.slackbot.Constants.DELIM_LEFT;
+import static com.omertron.slackbot.Constants.DELIM_RIGHT;
 import com.ullink.slack.simpleslackapi.*;
 import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
 import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
@@ -40,22 +44,18 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.omertron.bgg.enums.IncludeExclude;
-import com.omertron.slackbot.Constants;
-import static com.omertron.slackbot.Constants.DELIM_LEFT;
-import static com.omertron.slackbot.Constants.DELIM_RIGHT;
 
 /**
  *
  * @author stuar
  */
 public class BoardGameListener implements SlackMessagePostedListener {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(BoardGameListener.class);
     private static final BggApi BGG = new BggApi();
     private static final Pattern PAT_COMMAND;
     private static final Pattern PAT_ADMIN;
-    
+
     static {
         List<String> commands = new ArrayList<>();
         commands.add("search");
@@ -67,16 +67,16 @@ public class BoardGameListener implements SlackMessagePostedListener {
         HelpListener.addHelpMessage("user", "username", "Get information on a BGG user.");
         commands.add("coll");
         HelpListener.addHelpMessage("coll", "username", "Get a list of the owned games for a BGG user.");
-        
+
         String regex = new StringBuilder("(?i)")
                 .append("\\").append(DELIM_LEFT).append("\\").append(DELIM_LEFT)
                 .append("(").append(StringUtils.join(commands, "|")).append(")(?:\\W*)(.*)")
                 .append("\\").append(DELIM_RIGHT).append("\\").append(DELIM_RIGHT)
                 .toString();
-        
+
         LOG.info("Command Pattern: {}", regex);
         PAT_COMMAND = Pattern.compile(regex);
-        
+
         commands.clear();
         commands.add("quit");
         commands.add("restart");
@@ -90,11 +90,11 @@ public class BoardGameListener implements SlackMessagePostedListener {
                 .append("\\").append(DELIM_RIGHT).append("\\").append(DELIM_RIGHT)
                 .append(")?$")
                 .toString();
-        
+
         LOG.info("Admin Pattern: {}", regex);
         PAT_ADMIN = Pattern.compile(regex);
     }
-    
+
     @Override
     public void onEvent(SlackMessagePosted event, SlackSession session) {
         // Channel On Which Message Was Posted
@@ -112,9 +112,9 @@ public class BoardGameListener implements SlackMessagePostedListener {
         if (mCmd.matches()) {
             String command = mCmd.group(1).toUpperCase();
             String query = mCmd.group(2);
-            
+
             LOG.info("Command '{}', query '{}'", command, query);
-            
+
             switch (command) {
                 case "SEARCH":
                     botUpdateChannel(session, msgChannel, event);
@@ -143,7 +143,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
         if (mAdmin.matches()) {
             String command = mAdmin.group(1).toUpperCase();
             String params = mAdmin.group(2);
-            
+
             if (msgSender.isAdmin()) {
                 LOG.info("Command '{}' recieved from '{}' ({}) with params '{}'", command, msgSender.getUserName(), msgSender.getId(), params);
                 switch (command) {
@@ -165,7 +165,8 @@ public class BoardGameListener implements SlackMessagePostedListener {
     }
 
     /**
-     * Add a reaction to the message that called us and send the "typing..." indicator
+     * Add a reaction to the message that called us and send the "typing..."
+     * indicator
      *
      * @param session
      * @param msgChannel
@@ -187,10 +188,10 @@ public class BoardGameListener implements SlackMessagePostedListener {
     private void commandSearch(SlackSession session, SlackChannel msgChannel, String query) {
         StringBuilder response;
         SearchWrapper results;
-        
+
         LOG.info("Search request for '{}'", query);
         session.sendTyping(msgChannel);
-        
+
         try {
             results = BGG.searchBoardGame(query, false, false);
             LOG.info("Got {} results for '{}'", results.getTotal(), query);
@@ -203,31 +204,31 @@ public class BoardGameListener implements SlackMessagePostedListener {
             session.sendMessage(msgChannel, "Could not get information from BGG for " + query + ".\n Error: " + ex.getMessage());
             return;
         }
-        
+
         if (results.getItems() == null || results.getItems().isEmpty()) {
             session.sendMessage(msgChannel, "Could not find information from BGG for " + query);
             return;
         }
-        
+
         response = new StringBuilder("Found ");
         response.append(results.getTotal())
                 .append(" matches for *")
                 .append(query)
                 .append("*");
-        
+
         List<SlackAttachment> attachments = new ArrayList<>();
         for (Thing item : results.getItems()) {
             attachments.add(makeSimpleAttachment(item));
         }
-        
+
         SlackPreparedMessage spm = new SlackPreparedMessage.Builder()
                 .withMessage(response.toString())
                 .withAttachments(attachments)
                 .withUnfurl(false)
                 .build();
-        
+
         session.sendMessage(msgChannel, spm);
-        
+
     }
 
     /**
@@ -257,7 +258,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
             // Try converting the number
             bggId = NumberUtils.toInt(query, 0);
         }
-        
+
         if (bggId > 0) {
             try {
                 List<BoardGameExtended> results = BGG.getBoardGameInfo(bggId);
@@ -265,7 +266,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
                     session.sendMessage(msgChannel, "No results found for BGG ID " + bggId);
                     return;
                 }
-                
+
                 session.sendMessage(msgChannel, null, makeDetailedAttachment(results.get(0)));
             } catch (BggException ex) {
                 LOG.warn("Failed to get information from BGG on game ID {} - Query '{}'", bggId, query, ex);
@@ -274,7 +275,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
         } else {
             session.sendMessage(msgChannel, "Unable to find information for game with ID *" + query + "*");
         }
-        
+
     }
 
     /**
@@ -286,7 +287,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
      */
     private void commandUser(SlackSession session, SlackChannel msgChannel, String username) {
         LOG.info("COMMAND: User information for '{}'", username);
-        
+
         UserInfo result;
         try {
             result = BGG.getUserInfo(username);
@@ -295,12 +296,12 @@ public class BoardGameListener implements SlackMessagePostedListener {
             session.sendMessage(msgChannel, "Failed to get user information on " + username);
             return;
         }
-        
+
         if (result == null) {
             session.sendMessage(msgChannel, "No information found for username '" + username + "'");
             return;
         }
-        
+
         SlackAttachment sa = new SlackAttachment();
         sa.setFallback("User information on " + username);
         sa.setAuthorName(StringUtils.joinWith(" ", result.getFirstName(), result.getLastName()));
@@ -311,7 +312,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
         sa.addField("Location", StringUtils.joinWith(", ", result.getStateOrProvince(), result.getCountry()), true);
         sa.addField("Trade Rating", "" + result.getTradeRating(), true);
         sa.addField("Last Login", result.getLastLogin(), true);
-        
+
         boolean first = true;
         StringBuilder sb = new StringBuilder();
         if (!result.getHotList().isEmpty()) {
@@ -325,7 +326,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
             }
         }
         sa.addField("Hot List", sb.toString(), false);
-        
+
         first = true;
         sb = new StringBuilder();
         if (!result.getTopList().isEmpty()) {
@@ -339,7 +340,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
             }
         }
         sa.addField("Top List", sb.toString(), false);
-        
+
         session.sendMessage(msgChannel, result.getName(), sa);
     }
 
@@ -355,41 +356,41 @@ public class BoardGameListener implements SlackMessagePostedListener {
         try {
             List<IncludeExclude> includes = new ArrayList<>();
             includes.add(IncludeExclude.OWN);
-            result = BGG.getCollectionInfo(username, includes, null);
+            result = BGG.getCollectionInfo(username, null, includes, null);
         } catch (BggException ex) {
             LOG.warn("Failed to get collection for user '{}'", username, ex);
             session.sendMessage(msgChannel, "Failed to get collection for user " + username);
             return;
         }
-        
+
         if (result.getTotalItems() == 0) {
             session.sendMessage(msgChannel, "No information found for username '" + username + "'");
             return;
         }
-        
+
         int total = result.getTotalItems();
         int perPart = 75;
         int totalParts = (total + perPart - 1) / perPart;
         int count = 0;
         int partCount = 1;
-        
+
         session.sendMessage(msgChannel, username + " has " + total + " items in their collection. There will be " + totalParts + " parts listed.");
-        
+
         SlackAttachment sa = new SlackAttachment();
         sa.setFallback(username + "'s collection - part " + partCount + " of " + totalParts);
         sa.setAuthorName(username + "'s collection - part " + partCount + " of " + totalParts);
         sa.setAuthorLink("https://boardgamegeek.com/collection/user/" + username);
         sa.setColor("good");
-        
+
         List<SlackAttachment> collList = new ArrayList<>();
-        
+
         StringBuilder sb = new StringBuilder();
         for (CollectionItem item : result.getItems()) {
             count++;
             if (count >= perPart) {
                 sa.setText(sb.toString());
                 collList.add(sa);
-                
+
                 count = 0;
                 sb = new StringBuilder();
                 sa = new SlackAttachment();
@@ -399,7 +400,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
                 sa.setAuthorLink("https://boardgamegeek.com/collection/user/" + username);
                 sa.setColor("good");
             }
-            
+
             sb.append(String.format("%1$s (%2$s) - <https://boardgamegeek.com/boardgame/%3$d|%3$d>\n",
                     item.getName(),
                     item.getYearPublished(),
@@ -407,7 +408,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
         }
         sa.setText(sb.toString());
         collList.add(sa);
-        
+
         SlackPreparedMessage spm = new SlackPreparedMessage.Builder()
                 .withUnfurl(false)
                 .addAttachments(collList)
@@ -423,7 +424,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
      */
     private SlackAttachment makeSimpleAttachment(Thing game) {
         String year = game.getYearPublished() == null ? " (Unknown)" : " (" + game.getYearPublished() + ")";
-        
+
         SlackAttachment sa = new SlackAttachment();
         sa.setFallback("Information on " + game.getPrimaryName());
         sa.setAuthorName(game.getName() + year);
@@ -441,7 +442,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
     private SlackAttachment makeDetailedAttachment(BoardGameExtended game) {
         SlackAttachment sa = new SlackAttachment();
         String year = game.getYearPublished() == null ? " (Unknown)" : " (" + game.getYearPublished() + ")";
-        
+
         sa.setFallback("Information on " + game.getName());
         sa.setAuthorName(game.getName() + year);
         sa.setAuthorLink(Constants.BGG_GAME_LINK + game.getId());
@@ -455,7 +456,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
         sa.addField("Designer(s)", formatIdValue(game.getBoardGameDesigner()), true);
         sa.addField("Categories", formatIdValue(game.getBoardGameCategory()), true);
         sa.addField("Mechanics", formatIdValue(game.getBoardGameMechanic()), true);
-        
+
         return sa;
     }
 
@@ -468,7 +469,7 @@ public class BoardGameListener implements SlackMessagePostedListener {
     private String formatIdValue(List<IdValue> listToFormat) {
         StringBuilder result = new StringBuilder();
         boolean first = true;
-        
+
         for (IdValue idv : listToFormat) {
             if (first) {
                 first = false;
@@ -493,5 +494,5 @@ public class BoardGameListener implements SlackMessagePostedListener {
             return "https:" + link;
         }
     }
-    
+
 }
